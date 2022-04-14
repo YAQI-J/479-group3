e2c7469b01879dd48e73c949a8ec9e65621d5fea
Yaqi Jie <66143115+YAQI-J@users.noreply.github.com>
2022-04-13 21:57:02 -0500

    Update HTCondor.sh

--- HTCondor.sh
+++ HTCondor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -e 's/\t/ \+/g' -e 's/ \+/\n/g' <splited.$1> seded.$1
+sed -e ':a;N;$!ba;s/\t/ /g' -e 's/ /\n/g' splited.$1 > seded.$1
 
-sort <seded.$1> sorted.$1
+sort seded.$1 >sorted.$1
