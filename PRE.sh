1170083678d4a753afee87df6b3dd8034c2d4c69
Isabella Xue <71396377+isabellaxue@users.noreply.github.com>
2022-04-14 13:04:43 -0500

    changed: cat shakespeare/*/* >> allWords.txt
    Original: cat shakespeare*/* >> allWords.txt
    Now: cat shakespeare/*/* >> allWords.txt

--- PRE.sh
+++ PRE.sh
@@ -4,6 +4,6 @@ wget https://pages.stat.wisc.edu/~jgillett/479/CHTC/wordCounting/shakespeare.tar
 
 tar -xvf shakespeare.tar
 
-cat shakespeare*/* >> allWords.txt
+cat shakespeare/*/* >> allWords.txt
 
 split -n 5 -a 2 -d allWords.txt "splited."
