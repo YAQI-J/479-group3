6a7b17895b1db76b4f8c8fb2445e956fc5c0af20
Isabella Xue <71396377+isabellaxue@users.noreply.github.com>
2022-04-14 13:36:43 -0500

    Final version

--- PRE.sh
+++ PRE.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 wget https://pages.stat.wisc.edu/~jgillett/479/CHTC/wordCounting/shakespeare.tar
 
@@ -7,3 +7,5 @@ tar -xvf shakespeare.tar
 cat shakespeare/*/* >> allWords.txt
 
 split -n 5 -a 2 -d allWords.txt "splited."
+
+mkdir log error output
